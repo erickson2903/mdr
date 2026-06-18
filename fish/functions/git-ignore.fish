c9bebfd544cb516bdaf66ceb428619cfcb677af6
Erickson Dyeggo <ericksondyeggo@live.com>
2026-06-18 13:46:21 -0300

    Chore/Fix: Corrige funções do git e melhora a integração do TMUX

--- fish/functions/git-ignore.fish
+++ fish/functions/git-ignore.fish
@@ -1,7 +1,18 @@
 function git-ignore --description 'Mark file(s) as ignored by git (skip-worktree)'
     if test (count $argv) -eq 0
-        echo "Usage: git-ignore <file> [file...]"
+        echo "Usage: git-ignore <file|directory> [file|directory...]"
         return 1
     end
-    git update-index --skip-worktree $argv
+    set -l files
+    for arg in $argv
+        if test -d $arg
+            set files $files (git ls-files $arg)
+        else
+            set files $files $arg
+        end
+    end
+    if test (count $files) -eq 0
+        return
+    end
+    git update-index --skip-worktree $files
 end
--- fish/functions/git-track.fish
+++ fish/functions/git-track.fish
@@ -1,7 +1,18 @@
 function git-track --description 'Unmark file(s) as ignored by git (no-skip-worktree)'
     if test (count $argv) -eq 0
-        echo "Usage: git-track <file> [file...]"
+        echo "Usage: git-track <file|directory> [file|directory...]"
         return 1
     end
-    git update-index --no-skip-worktree $argv
+    set -l files
+    for arg in $argv
+        if test -d $arg
+            set files $files (git ls-files $arg)
+        else
+            set files $files $arg
+        end
+    end
+    if test (count $files) -eq 0
+        return
+    end
+    git update-index --no-skip-worktree $files
 end
